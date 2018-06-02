6af3e4442df71d3cc53a68462ac9ad3c4b6c0103
phyushin <phyushin@phyubox.com>
2018-06-02 21:50:48 +0100

    Initial Commit

---
+++ src/main.cpp
@@ -0,0 +1,33 @@
+#include <Arduino.h>
+const int steadyHandPin = 12;
+const int resetPin = 11;
+const int alarmPin = 10;
+bool latch;
+void setup() {
+    Serial.begin(9600);
+    //Init Pins
+    pinMode(alarmPin, OUTPUT);
+    pinMode(resetPin, INPUT);
+    pinMode(steadyHandPin, INPUT);
+    //End Pins
+    latch = false;
+}
+
+void loop() {
+  if(digitalRead(steadyHandPin)==HIGH){
+    Serial.println("You touched the wire! Oh No!");
+    latch = true;
+  }
+
+  if(digitalRead(resetPin) == HIGH){
+    Serial.println("So you're going to try again!");
+    latch = false;
+  }
+
+  if (latch){
+    digitalWrite(alarmPin, HIGH);
+  }else{
+    digitalWrite(alarmPin, LOW);
+  }
+
+}
